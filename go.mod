ef27e1868b928b05a2023cd7e43931ea157df205
rambabu <rchalicham@gmail.com>
2021-10-01 22:17:43 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gihub.com/rchalicham/gotraining
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"gihub.com/rchalicham/gotraining/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe(":9090", sm)
+}
